02268234737a978537860da8804a53f8ceb271fc
Adriel Werlich <adrielwerlich@gmail.com>
2021-05-14 10:46:35 -0300

    gh-pages11

---
+++ precache-manifest.49948678e60f51f8786a4a363649610d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31f5431ef660bc4701f3",
+    "url": "/css/app.f85f00c1.css"
+  },
+  {
+    "revision": "0323a0a1f9efbd57d570",
+    "url": "/css/chunk-vendors.095f1253.css"
+  },
+  {
+    "revision": "e5c86955ae9972167e6fb93730c11b76",
+    "url": "/favicon-poke.png"
+  },
+  {
+    "revision": "269835197dab99d7681ba6317a716c1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edc2151827203401b7c1",
+    "url": "/js/about.e763de23.js"
+  },
+  {
+    "revision": "31f5431ef660bc4701f3",
+    "url": "/js/app.e27e6464.js"
+  },
+  {
+    "revision": "0323a0a1f9efbd57d570",
+    "url": "/js/chunk-vendors.d04d0978.js"
+  },
+  {
+    "revision": "9444b3dbe38a87c027c3874eb328a968",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
