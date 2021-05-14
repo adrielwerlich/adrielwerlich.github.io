e97dcb45068a893a5e1cc7b9212a4596693856c9
Adriel Werlich <adrielwerlich@gmail.com>
2021-05-14 10:22:57 -0300

    gh-pages9

---
+++ precache-manifest.3c4492ae647f8c7d6503eb61342ec390.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b682b35c8e9cef67584e",
+    "url": "/poke-list/css/app.f85f00c1.css"
+  },
+  {
+    "revision": "080243437151431bc977",
+    "url": "/poke-list/css/chunk-vendors.095f1253.css"
+  },
+  {
+    "revision": "e5c86955ae9972167e6fb93730c11b76",
+    "url": "/poke-list/favicon-poke.png"
+  },
+  {
+    "revision": "0fe7da6a5f70dbbc86422815f084d861",
+    "url": "/poke-list/index.html"
+  },
+  {
+    "revision": "edc2151827203401b7c1",
+    "url": "/poke-list/js/about.e763de23.js"
+  },
+  {
+    "revision": "b682b35c8e9cef67584e",
+    "url": "/poke-list/js/app.ffc33475.js"
+  },
+  {
+    "revision": "080243437151431bc977",
+    "url": "/poke-list/js/chunk-vendors.5ec57b40.js"
+  },
+  {
+    "revision": "9444b3dbe38a87c027c3874eb328a968",
+    "url": "/poke-list/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/poke-list/robots.txt"
+  }
+]);
